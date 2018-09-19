2cc6513fe71198bc7da50543d315d4e25e218342
Jonsada <Jonsada@192.168.100.15>
2018-09-19 17:09:26 +0530

    comm

--- Jenkins/src/Example/Test1.java
+++ Jenkins/src/Example/Test1.java
@@ -14,6 +14,7 @@ public class Test1 {
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.flipkart.com/");
 		driver.manage().window().maximize();
+		System.out.println("Applicaion opened");
 		
 	}
 	
